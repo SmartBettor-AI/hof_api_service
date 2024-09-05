4f21d5152ffe3ee5711789f55d001470ef09792b
youngfeiler <stefantfeiler@gmail.com>
2024-09-05 14:12:48 -0600

    more cors stuf

--- app.py
+++ app.py
@@ -36,7 +36,9 @@ logger = logging.getLogger(__name__)
 
 def create_app():
     app = Flask(__name__, template_folder='static/templates', static_folder='static')
-    CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+    CORS(app, resources={r"/*": {"origins": ["https://homeoffightpicks.com", "https://localhost:3000"]}}, supports_credentials=True)
+
+
     Compress(app)
     # TODO: Put this key in the secret file
     app.secret_key = 'to_the_moon'
