7aa534026fbd3ea9cf65c7044889fe8359d7545c
micahcb <micahcb@umich.edu>
2024-09-27 18:26:13 -0600

    removed bovada

--- functionality/scraper.py
+++ functionality/scraper.py
@@ -600,6 +600,7 @@ class fightOddsIOScraper(MMAScraper):
 
             # Step 2: Drop rows where all specified columns are NaN
             df = df.dropna(subset=columns_to_check, how='all')
+            df = df.drop(columns=['Bovada'])
 
             name_and_date = self.find_fight_name_and_date(soup)
             df['fight_name'] = name_and_date[0]
