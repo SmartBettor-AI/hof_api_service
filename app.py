0502c163e2e3efc29384261769f056d682f938e0
micahcb <micahcb@umich.edu>
2024-10-08 12:39:01 -0600

    add home of fight picks to the origins

--- app.py
+++ app.py
@@ -46,10 +46,7 @@ logger = logging.getLogger(__name__)
 
 def create_app():
     app = Flask(__name__, template_folder='static/templates', static_folder='static')
-    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
-
-
-
+    CORS(app, resources={r"/*": {"origins": ["https://homeoffightpicks.com", "http://localhost:3000"]}}, supports_credentials=True)
 
     Compress(app)
     # TODO: Put this key in the secret file
