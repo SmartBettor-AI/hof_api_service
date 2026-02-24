f871e7ba7fed3a188bf70e9691f4a08faf25fa40
Stefan Feiler <stefan@Stefans-MacBook-Pro.local>
2026-02-23 18:34:45 -0800

    Start script

--- functionality/bash_scripts/scraper.sh
+++ functionality/bash_scripts/scraper.sh
@@ -6,12 +6,15 @@ run_python_process() {
     local script=$2
     local log_file=$3
     # Using absolute path and ensuring stdout and stderr are both redirected to the log file
-    nohup python3 $script > $log_file 2>&1 &
+    nohup python3 "$script" > "$log_file" 2>&1 &
 }
 
 # Use absolute paths to avoid relative path issues
 script_path="/home/ec2-user/hof_api_service/functionality/scraper.py"
 log_file="/home/ec2-user/hof_api_service/functionality/bash_scripts/scraper.log"
+
+# Ensure log directory exists
+mkdir -p "$(dirname "$log_file")"
 
 # Run the Python process (omit taskset if unnecessary)
 run_python_process "0,1" "$script_path" "$log_file"
