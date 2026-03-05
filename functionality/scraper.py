93c9894fe2f180ed028671556ac462180741a72e
micahcb <micahcb@umich.edu>
2026-03-05 11:25:11 -0800

    Add proxy to get_odds_per_page for Cloudflare

--- functionality/scraper.py
+++ functionality/scraper.py
@@ -479,7 +479,13 @@ class fightOddsIOScraper(MMAScraper):
                     headless=True,
                     args=["--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu"]
                 )
-                page = browser.new_page()
+                proxy = {
+                    "server": "http://geo.iproyal.com:12321",
+                    "username": "qyM9d25gbxsra2UP",
+                    "password": "e5ChXHYHhTTdC7Hu_streaming-1",
+                }
+                context = browser.new_context(proxy=proxy)
+                page = context.new_page()
                 page.goto(url)
 
                 try:
